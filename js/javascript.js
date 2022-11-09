5bf693861ef673565ea455c60d56a6eaa4cf7248
T140K <darek1430143@gmail.com>
2022-11-09 12:30:21 +0100

    KOMPETENS is from json

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,19 @@
-
-
 fetch('JSON/cv.JSON')
    .then(response => response.json())
    .then(data => {
-      document.querySelector('.title-text').innerHTML = data.pageTitle.cvTitle;
+      console.log(data);
+      document.querySelector('.title-text').innerHTML = data.cvTitle;
+
+
+      document.querySelector('.kompetens-title').innerHTML = data.kompetensTitle;
+
+      for (i = 0; i < data.kompetens.length; i++) {
+         document.querySelector('.sidebar-cv').innerHTML +=
+         `
+         <p class="sidebar-cv-text">
+         
+         ${data.kompetens[i]} 
+         <p/>
+         `
+      }
    })
